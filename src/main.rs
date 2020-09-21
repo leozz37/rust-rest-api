8d688b352ac8a4ca6688ecbc9f835e148cea70b9
leozz37 <leonardoaugusto287@gmail.com>
2020-09-21 17:19:39 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::{Json, serde_json::Value};
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[get("/")]
+fn read() -> Json<serde_json::Value> {
+    Json(json!([
+        "hero 1", 
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<serde_json::Value> {
+    Json(json!({"status": "ok"}))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
+        .launch();
+}
